cfd721865c20746690e1b8ea13670ad8bc36ac98
no7dw <no7david@gmail.com>
2017-07-18 15:20:27 +0800

    add ab test code and result

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # paginate demo
+
+### ab test result
+hello world 
+
+ - 3000~4000 rps
+
+with db query (ab -n 1000 -c 200 http://localhost:3000/)
+
+ - 850 rps
+
+hardware :2016 RMBP
--- app.js
+++ app.js
@@ -3,6 +3,20 @@ let db = dbs.get('finance')
 let User = require(`./model/User`)
 let UserModel = new User(db).init()
 let option = {skip:2, limit:1}
-UserModel.find({}, {name:1}, option, function(err, result){
-  console.log(result)
-})
+
+const Koa = require('koa');
+var Router = require('koa-router');
+const app = new Koa();
+var router = new Router();
+
+//ctx.body = 'hello world';
+router.get('/' , async function(ctx, next) {
+    const ret = await UserModel.find({}, {name:1}, option)
+    ctx.body = ret
+});
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(3000);
+
