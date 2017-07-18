bbe1ffc2303bc45c7930cd45b5b323baf2e047d4
no7dw <no7david@gmail.com>
2017-07-18 15:33:15 +0800

    depart 2 router

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # paginate demo
 
 ### ab test result
+
+  ab -n 2000 -c 250 http://localhost:3000/h
+
 hello world 
 
  - 3000~4000 rps
 
-with db query (ab -n 1000 -c 200 http://localhost:3000/)
+with db query (ab -n 1000 -c 200 http://localhost:3000/user?name=wade)
 
- - 850 rps
+ - 600~850 rps
 
 hardware :2016 RMBP
--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const dbs = require('./config').dbs
 let db = dbs.get('finance')
 let User = require(`./model/User`)
 let UserModel = new User(db).init()
-let option = {skip:2, limit:1}
+let option = {skip:0, limit:1}
 
 const Koa = require('koa');
 var Router = require('koa-router');
 const app = new Koa();
 var router = new Router();
 
-//ctx.body = 'hello world';
-router.get('/' , async function(ctx, next) {
-    const ret = await UserModel.find({}, {name:1}, option)
-    ctx.body = ret
+router.get('/h' ,  function(ctx, next) {
+  ctx.body = 'hello world';
+});
+
+//
+router.get('/user' , async function(ctx, next) {
+  const ret = await UserModel.find({name: ctx.query.name}, {name:1}, option)
+  ctx.body = ret
+  next()
 });
 
 app.use(router.routes())
